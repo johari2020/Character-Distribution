77afba0163a0be8488a66fb45593ace2883740ed
johari2020 <42872004+johari2020@users.noreply.github.com>
2018-10-12 11:47:49 -0400

    Updated from Brython Server: 10/12/2018 11:47:49 AM

--- distribution.py
+++ distribution.py
@@ -1,7 +1,7 @@
 """
 distribution.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Johariajwang
+Credit: megsnyder
 
 Assignment:
 
@@ -37,39 +37,11 @@ Notice about this example:
 * Letters that do not occur in the text are not listed in the output at all.
 """
 
-st = input("Please enter a string of text (the bigger the better): ") 
+rr = input("Please enter a string of text (the bigger the better): ") 
 
-'''
-import string
-print(str.ascii_lowercase)
-'''
-'''
-a = (st.count('a'))
-b = (st.count('b'))
-c = (st.count('c'))
-d = (st.count('d'))
-e = (st.count('e'))
-f = (st.count('af'))
-g = (st.count('g'))
-h = (st.count('h'))
-i = (st.count('i'))
-j = (st.count('j'))
-k = (st.count('k'))
-l = (st.count('l'))
-m = (st.count('m'))
-n = (st.count('n'))
-o = (st.count('o'))
-q = (st.count('p'))
-r = (st.count('q'))
-s = (st.count('r'))
-t = (st.count('s'))
-u = (st.count('t'))
-v = (st.count('u'))
-w = (st.count('v'))
-x = (st.count('w'))
-y = (st.count('x'))
-z = (st.count('y'))
-'''
+st=rr.lower
+
+print(st)
 i=len(st)
 l=list(range(1,i))
 print(l)
@@ -127,29 +99,5 @@ while i > 0:
         print("y"*(st.count('y')))
     if st.count('z')==i:
         print("z"*(st.count('z')))
-        
     i-=1
-'''
-print("d"*st.count('d'))
-    print("e"*st.count('e'))
-    print("f"*st.count('af'))
-    print("g"*st.count('g'))
-    print("h"*st.count('h'))
-    print("i"*st.count('i'))
-    print("j"*st.count('j'))
-    print("k"*st.count('k'))
-    print("d"*st.count('l'))
-    print("m"*st.count('m'))
-    print("n"*st.count('n'))
-    print("o"*st.count('o'))
-    print("p"*st.count('q'))
-    print("r"*st.count('r'))
-    print("s"*st.count('s'))
-    print("t"*st.count('t'))
-    print("u"*st.count('u'))
-    print("v"*st.count('v'))
-    print("w"*st.count('w'))
-    print("x"*st.count('x'))
-    print("y"*st.count('y'))
-    print("z"*st.count('z'))
-'''
+
